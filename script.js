9a9de4ba25cc7a2b89bc573ded3986a3035d8c4a
Thulhid <153322849+Thulhid@users.noreply.github.com>
2024-03-17 18:15:32 +0530

    V3_(Refactored_CommentRemoval)
    Comments Removal: Removed commented-out code to keep the file clean and focused.

--- script.js
+++ script.js
@@ -17,10 +17,8 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess, typeof guess);
 
   if (!guess) {
-    //document.querySelector('.message').textContent = '⛔ No number!';
     displayMessage('⛔ No number!');
   } else if (guess === secretNumber) {
-    //document.querySelector('.message').textContent = '🎉 Correct Number!';
     displayMessage('🎉 Correct Number!');
 
     if (highScore <= score && score > 1) {
@@ -33,39 +31,15 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.number').style.width = '30rem';
   } else if (guess !== secretNumber) {
     if (score > 1) {
-      //document.querySelector('.message').textContent =
-      //guess > secretNumber ? '📉 Too high!' : '📈 Too low!';
       displayMessage(guess > secretNumber ? '📉 Too high!' : '📈 Too low!');
       --score;
 
-      //document.querySelector('.score').textContent = score;
       displayScore(score);
     } else {
-      //document.querySelector('.message').textContent = '💥 You lost the game!';
       displayMessage('💥 You lost the game!');
-      //document.querySelector('.score').textContent = 0;
+
       displayScore(0);
     }
-    // } else if (guess > secretNumber) {
-    //   if (score > 1) {
-    //     document.querySelector('.message').textContent = '📉 Too high!';
-    //     --score;
-
-    //     document.querySelector('.score').textContent = score;
-    //   } else {
-    //     document.querySelector('.message').textContent = '💥 You lost the game!';
-    //     document.querySelector('.score').textContent = 0;
-    //   }
-    // } else if (guess < secretNumber) {
-    //   if (score > 1) {
-    //     document.querySelector('.message').textContent = '📈 Too low!';
-    //     --score;
-
-    //     document.querySelector('.score').textContent = score;
-    //   } else {
-    //     document.querySelector('.message').textContent = '💥 You lost the game!';
-    //     document.querySelector('.score').textContent = 0;
-    //   }
   }
 });
 
@@ -79,6 +53,5 @@ document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').textContent = '?';
   document.querySelector('.number').style.width = '15rem';
   document.querySelector('.guess').value = '';
-  //document.querySelector('.message').textContent = 'Start guessing...';
   displayMessage('Start guessing...');
 });
